48375f7e3becde847978a240975f73536af38945
罗一帅 <luoyishuai@lattebank.com>
2017-07-13 09:33:47 +0800

    Add spring test for rest

--- src/main/java/com/yishuailuo/mywebproject/controller/HelloWorldController.java
+++ src/main/java/com/yishuailuo/mywebproject/controller/HelloWorldController.java
@@ -3,7 +3,6 @@ package com.yishuailuo.mywebproject.controller;
 import com.yishuailuo.mywebproject.service.HelloWorldService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,8 +15,7 @@ public class HelloWorldController {
     private HelloWorldService helloWorldService;
 
     @GetMapping("/hello")
-    @ResponseBody
-    public String returWelcome() {
+    public String returnWelcome() {
         return helloWorldService.returnWelcome();
     }
 }
--- src/test/java/com/yishuailuo/mywebproject/controller/HelloWorldControllerTest.java
+++ src/test/java/com/yishuailuo/mywebproject/controller/HelloWorldControllerTest.java
@@ -1,9 +1,55 @@
 package com.yishuailuo.mywebproject.controller;
 
+import com.yishuailuo.mywebproject.service.HelloWorldService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 /**
  * Created by luoyishuai on 17/7/12.
  */
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:applicationContext-unitTest.xml")
+@WebAppConfiguration
 public class HelloWorldControllerTest {
 
+    private MockMvc mockMvc;
+
+    @Autowired
+    private HelloWorldService helloWorldServiceMock;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setUp() {
+        Mockito.reset(helloWorldServiceMock);
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void test() throws Exception{
+        String resultString = "return welcome";
+        when(helloWorldServiceMock.returnWelcome()).thenReturn(resultString);
+
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", is("return welcome")));
+
+    }
 }
