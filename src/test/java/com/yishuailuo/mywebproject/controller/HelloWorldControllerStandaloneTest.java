5879f630e228f71768f4a1b378fa61875a915860
罗一帅 <luoyishuai@lattebank.com>
2017-07-28 15:06:24 +0800

    remove unused test annotation with standalone controller

--- src/test/java/com/yishuailuo/mywebproject/controller/HelloWorldControllerStandaloneTest.java
+++ src/test/java/com/yishuailuo/mywebproject/controller/HelloWorldControllerStandaloneTest.java
@@ -3,30 +3,21 @@ package com.yishuailuo.mywebproject.controller;
 import com.yishuailuo.mywebproject.service.HelloWorldService;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.hamcrest.core.Is.is;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * Created by luoyishuai on 17/7/17.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-// @ContextConfiguration 指定加载的 spring 配置文件（一个或多个）的位置, locations=xml配置文件， classes=class配置文件
-@ContextConfiguration(locations = "classpath:applicationContext-unitTest.xml")
-@WebAppConfiguration
+
 public class HelloWorldControllerStandaloneTest {
 
     private MockMvc mockMvc;
